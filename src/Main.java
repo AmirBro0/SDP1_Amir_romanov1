438b4c641a5cf8b0eb930c8248f6df83e458974a
AmirBro0 <_@gmail.com>
2026-09-20 11:10:00 +0500

    feat: add Main class demonstration

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== 1. Custom PC Build via Builder ===");
+        Computer customPc = new ComputerBuilder("AMD Ryzen 5 5600", "B550", 16, 600)
+                .withGpu("NVIDIA RTX 3060")
+                .withStorage("NVMe SSD", 512)
+                .enableWifi()
+                .withCpuCores(6)
+                .build();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        System.out.println("CPU: " + customPc.getCpu());
+        System.out.println("GPU: " + customPc.getGpu());
+        System.out.println("RAM: " + customPc.getRamGb() + "GB");
+        System.out.println("Storage: " + customPc.getStorage().type() + " " + customPc.getStorage().capacityGb() + "GB");
+        System.out.println("Wi-Fi Enabled: " + customPc.isHasWifi());
+
+        System.out.println("\n=== 2. Preset Configurations via Director ===");
+        ComputerDirector director = new ComputerDirector();
+
+        Computer officePc = director.constructOfficePc();
+        System.out.println("Office PC CPU: " + officePc.getCpu() + " | Cores: " + officePc.getCpuCores());
+
+        Computer gamingPc = director.constructGamingPc();
+        System.out.println("Gaming PC GPU: " + gamingPc.getGpu() + " | Water Cooling: " + gamingPc.isHasWaterCooling() + " 🍌");
+
+        Computer workstation = director.constructWorkstationPc();
+        System.out.println("Workstation RAM: " + workstation.getRamGb() + "GB | PSU: " + workstation.getPowerSupplyWattage() + "W");
     }
 }
